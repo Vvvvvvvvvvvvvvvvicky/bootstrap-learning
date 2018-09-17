db61ea12b87488a8a9b47422236eb37a1e6cbe9f
vic <951386177@qq.com>
2018-09-17 16:36:00 +0800

    update

---
+++ template/index/index.js
@@ -0,0 +1,20 @@
+// $(function () {
+//     $(".sidebar").find("li").each(function () {
+//         var a = $(this).find("a:first")[0];
+//         if ($(a).attr("href") === location.pathname) {
+//             $(this).addClass("active");
+//         } else {
+//             $(this).removeClass("active");
+//         }
+//     });
+// });
+
+// $(document).ready(function(){
+//     //这是给div的一个初始页面
+//     $(".main").html('<object type="text/html" data="test.html" width="100%" height="500px"></object>');
+// })
+
+//  function menuUrl() {
+//      $("#main").html('<object type="text/html" data="test.html" width="100%" height="500px"></object>');
+//  };
+
---
+++ test.js
@@ -0,0 +1,7 @@
+
+
+$(document).ready(function () {
+ 
+            // $('.test').load('index.html');
+            $('.test').html('<object type="text/html" data="index.html" width="100%" height="100%"></object>')
+        });
